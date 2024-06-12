ea64c004a54b84216e784808bb77e294f880aba5
Eduardo-BG <lalobegar@gmail.com>
2024-06-11 19:42:54 -0600

    Jump and ground added to game_V1_4_player.py. Also a way to exit the game when the snail hits the player

--- intro/game_v1_4_player.py
+++ intro/game_v1_4_player.py
@@ -1,4 +1,7 @@
 
+# keyboard input
+# jump and fall
+
 import pygame
 from sys import exit
 
@@ -19,12 +22,21 @@ snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        #if event.type == pygame.KEYUP:
+        #    print('key up')
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     pygame.draw.rect(screen, '#c0e8ec', score_rect)
@@ -32,9 +44,23 @@ while True:
     screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
-    if snail_rect.right <= 0: snail_rect.left = 800
+    if snail_rect.right <= 0: snail_rect.left = 800 
     screen.blit(snail_surf, snail_rect)
+
+    # Player   
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300: player_rect.bottom = 300
     screen.blit(player_surf, player_rect) 
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #    print('jump')
+
+    # Collision
+    if snail_rect.colliderect(player_rect):
+         pygame.quit()
+         exit()
 
     pygame.display.update()
     clock.tick(60)
